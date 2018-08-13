5f9e61e61957a02b4858eaa6bd8d94e79d7d33c7
Guillaume Biton <guillaume.biton@free.fr>
2018-08-13 10:24:17 +0200

    Update 2018-08-13T08:24:15.178Z

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,18 @@
 routes = [
     {
       path: '/',
-      componentUrl: './pages/stories.html',
+      async: function (routeTo, routeFrom, resolve, reject) {
+        var self = this;
+        var app = self.app;
+
+        // Get hnapi data
+        app.request.json(app.data.apiUrl, function (data) {
+          app.data.api = data;
+          resolve({
+            componentUrl: './pages/stories.html',
+          })
+        })
+      }
     },
     {
       path: '/about/',

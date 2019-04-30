d1a4b2f280dc5f9fc140157c5d3172d02ed86b61
Guillaume Biton <guillaume.biton@free.fr>
2019-04-30 09:50:43 +0200

    update demo

--- docs/precache-manifest.ee64b368b11e56fbe88233e61274b033.js
+++ docs/precache-manifest.ee64b368b11e56fbe88233e61274b033.js
@@ -1,27 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aba562baa8dc57bd9d89",
+    "revision": "580b1d9ab465c47cb9b927fd01664225",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0f566ab8d6721329c14",
     "url": "css/app.css"
-  },
-  {
-    "revision": "aba562baa8dc57bd9d89",
-    "url": "js/app.js"
-  },
-  {
-    "revision": "19158ff09eb907432a7ccacd074ef9d5",
-    "url": "fonts/Framework7Icons-Regular.woff2"
-  },
-  {
-    "revision": "5fce7c1238bad823bee064bd127fe4cd",
-    "url": "fonts/Framework7Icons-Regular.woff"
-  },
-  {
-    "revision": "570eb83859dc23dd0eec423a49e147fe",
-    "url": "fonts/MaterialIcons-Regular.woff2"
-  },
-  {
-    "revision": "012cf6a10129e2275d79d6adac7f3b02",
-    "url": "fonts/MaterialIcons-Regular.woff"
   },
   {
     "revision": "28db829912bb402df5b1e6fb16f1908c",
@@ -32,16 +16,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/Framework7Icons-Regular.ttf"
   },
   {
-    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
-    "url": "fonts/MaterialIcons-Regular.ttf"
+    "revision": "5fce7c1238bad823bee064bd127fe4cd",
+    "url": "fonts/Framework7Icons-Regular.woff"
+  },
+  {
+    "revision": "19158ff09eb907432a7ccacd074ef9d5",
+    "url": "fonts/Framework7Icons-Regular.woff2"
   },
   {
     "revision": "e79bfd88537def476913f3ed52f4f4b3",
     "url": "fonts/MaterialIcons-Regular.eot"
   },
   {
-    "revision": "580b1d9ab465c47cb9b927fd01664225",
-    "url": "./index.html"
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.ttf"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "a0f566ab8d6721329c14",
+    "url": "js/app.js"
   },
   {
     "revision": "c2ac2c600888eebb2eb57f8b310bfec6",
@@ -50,10 +50,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "436babe476e12a14eac928bc1d47cdfc",
     "url": "static/.DS_Store"
-  },
-  {
-    "revision": "84b7172c8a57f9b721c3fd60c217280b",
-    "url": "static/icons/favicon.ico"
   },
   {
     "revision": "4a30eea30e507a8122bbe44c05233fdd",
@@ -68,14 +64,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "static/icons/152x152.png"
   },
   {
-    "revision": "56a6f087b2509df23805162066e27c3f",
-    "url": "static/icons/apple-touch-icon.png"
-  },
-  {
-    "revision": "4a30eea30e507a8122bbe44c05233fdd",
-    "url": "static/icons/favicon.png"
-  },
-  {
     "revision": "956df353394a14efbdae3c58150d16c8",
     "url": "static/icons/192x192.png"
   },
@@ -86,5 +74,17 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fdb0da737b309ae166828b5bb8f0803d",
     "url": "static/icons/512x512.png"
+  },
+  {
+    "revision": "56a6f087b2509df23805162066e27c3f",
+    "url": "static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "84b7172c8a57f9b721c3fd60c217280b",
+    "url": "static/icons/favicon.ico"
+  },
+  {
+    "revision": "4a30eea30e507a8122bbe44c05233fdd",
+    "url": "static/icons/favicon.png"
   }
 ]);
